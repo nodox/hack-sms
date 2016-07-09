5301cb22fba05c5145f0b7841cdb937611b5d5b6
nodox <snatera09@mgail.com>
2016-07-08 21:38:35 -0400

    add login page

--- app/app.ts
+++ app/app.ts
@@ -3,6 +3,8 @@ import {ionicBootstrap, Platform, MenuController, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
 import {ListPage} from './pages/list/list';
+import {LoginPage} from './pages/login/login';
+
 
 
 @Component({
@@ -24,7 +26,8 @@ class MyApp {
     // set our app's pages
     this.pages = [
       { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'My First List', component: ListPage }
+      { title: 'My First List', component: ListPage },
+      { title: 'Login', component: LoginPage }
     ];
   }
 
